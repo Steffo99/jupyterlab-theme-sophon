53d11090d5d16ce2127494ffc3b2382d38c617e6
Stefano Pigozzi <me@steffo.eu>
2021-09-01 01:31:14 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the jupyterlab_theme_sophon extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab_theme_sophon',
+  requires: [IThemeManager],
+  autoStart: true,
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension jupyterlab_theme_sophon is activated!');
+    const style = 'jupyterlab_theme_sophon/index.css';
+
+    manager.register({
+      name: 'jupyterlab_theme_sophon',
+      isLight: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default extension;
